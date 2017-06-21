79a3c2fbd26589c7e80ae8e54efd82cf24285d51
minu youn <anomie7777@gmail.com>
2017-06-21 10:31:51 +0900

    DataStructure--Chapter2] Code05 완료

--- Chapter2/src/Chaper1/Code05.java
+++ Chapter2/src/Chaper1/Code05.java
@@ -16,30 +16,33 @@ public class Code05 {
 				rects[n].lu.y = source.nextInt();
 				rects[n].width = source.nextInt();
 				rects[n++].height = source.nextInt();
-			}
-			bubbleSort();
-			for(int i = 0; i < n; i++){
-				System.out.println(rects[i].lu.x + " "
-								   + rects[i].lu.y + " "
-								   + rects[i].width + " "
-								   + rects[i].height);
-			}
+			}	
 			source.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("No data file exists.");
 			System.exit(1);
 		}
+		bubbleSort();
+		for(int i = 0; i < n; i++){
+			System.out.println(rects[i].lu.x + " "
+							   + rects[i].lu.y + " "
+							   + rects[i].width + " "
+							   + rects[i].height);
+		}
 	}
 	private static void bubbleSort() {
 		for(int i = n-1; i > 0; i--){
-			for(int j = 0; j < i; i++){
-				if( (rects[j].width*rects[j].height) > (rects[j+1].width*rects[j+1].height)){
-					MyRectangle1 tmp = rects[j];
+			for(int j = 0; j < i; j++){
+				if( calcArea(rects[j]) > calcArea(rects[j+1]) ){
+					MyRectangle1 tmp = rects[j]; 
 					rects[j] = rects[j+1];
 					rects[j+1] = tmp;
 				}
 			}
 		}
+	}
+	private static int calcArea(MyRectangle1 r){
+		return r.width * r.height;
 	}
 
 }
